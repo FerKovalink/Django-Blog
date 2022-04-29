60a37e3834d187d12ce6803813f484e5d54d7286
Fer Kova <ferkovalink@gmail.com>
2022-04-29 02:19:17 -0300

    buscador

--- blog/views.py
+++ blog/views.py
@@ -1,34 +1,29 @@
-from django.shortcuts import render
-from django.views import generic
 from .models import Post
+from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 
 def about(request):
     return render(request, 'about.html')
 
 
-class PostList(generic.ListView):
+class PostList(ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_at')
     template_name = 'index.html'
 
 
-class PostDetail(generic.DetailView):
+class PostDetail(DetailView):
     model = Post
     template_name = 'post.html'
 
 
-class PostLista(LoginRequiredMixin, ListView):
-      model = Post 
-      template_name = 'post_list.html'
-
-
 class PostDetalle(DetailView):
       model = Post
-      template_name = 'post_detalle.html'
+      template_name = 'post.html'
 
 
 class PostCreacion(CreateView):
@@ -46,3 +41,13 @@ class PostUpdate(UpdateView):
 class PostDelete(DeleteView):
       model = Post
       success_url = 'post_confirm_delete.html'
+
+
+def buscar(request):
+      if  request.GET['title']:
+            title = request.GET['title'] 
+            post = Post.objects.filter(title__icontains=title)
+            return render(request, 'base.html', {'post':post, 'title':title})
+
+      else: 
+            return HttpResponse('No se encuentra {title}')
