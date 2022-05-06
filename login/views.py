4e057e6720b1c4fa2427388b1028ab5fceffe69c
Fer Kova <ferkovalink@gmail.com>
2022-05-06 00:47:32 -0300

    about

--- login/views.py
+++ login/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate
 from login.forms import UserRegisterForm, UserEditForm
 
